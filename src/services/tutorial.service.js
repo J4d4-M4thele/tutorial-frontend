d009837afedcae8e0745e01bfeb7b01bf974480e
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-02 15:02:21 +0200

    first commit

---
+++ src/services/tutorial.service.js
@@ -0,0 +1,33 @@
+import http from "../http-common";
+
+class TutorialDataService {
+  getAll() {
+    return http.get("https://tutorial-backend.onrender.com/api/tutorials");
+  }
+
+  get(id) {
+    return http.get(`https://tutorial-backend.onrender.com/api/tutorials/${id}`);
+  }
+
+  create(data) {
+    return http.post("https://tutorial-backend.onrender.com/api/tutorials", data);
+  }
+
+  update(id, data) {
+    return http.put(`https://tutorial-backend.onrender.com/api/tutorials/${id}`, data);
+  }
+
+  delete(id) {
+    return http.delete(`https://tutorial-backend.onrender.com/api/tutorials/${id}`);
+  }
+
+  deleteAll() {
+    return http.delete(`https://tutorial-backend.onrender.com/api/tutorials`);
+  }
+
+  findByTitle(title) {
+    return http.get(`https://tutorial-backend.onrender.com/api/tutorials?title=${title}`);
+  }
+}
+
+export default new TutorialDataService();
